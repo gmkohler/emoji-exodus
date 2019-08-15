b804462781bce77257e95f5a5087c8427f281dcd
Gregory Kohler and Patrick Vilhena <git+kohler+pvilhena@squareup.com>
2019-08-15 11:56:01 -0700

    🧠 memoize API queries
    sets us up for multiple emoji to be added while only fetching data once.

--- emoji/slack_emoji_client.py
+++ emoji/slack_emoji_client.py
@@ -1,11 +1,14 @@
 import os
 import sys
 import slack
+from image import image_client
 
 
 class SlackEmojiClient:
     def __init__(self, token):
         self.client = slack.WebClient(token=token)
+        self._emoji_dict = None
+        self._hashes_for_emoji_images = None
 
     def add_emoji(self, image_file, name):
         """ `image_file` can be a local path to a file or a sublcass of `IOBase`. """
@@ -26,4 +29,21 @@ class SlackEmojiClient:
         return self.client.api_call("emoji.remove", data={"name": name, "mode": "data"})
 
     def emoji_dict(self):
-        return self.client.emoji_list().data['emoji']
+        if self._emoji_dict is None:
+            self._emoji_dict = self.client.emoji_list().data['emoji']
+
+        return self._emoji_dict
+
+    def hashes_for_emoji_images(self):
+        if self._hashes_for_emoji_images is None:
+            self._hashes_for_emoji_images = {}
+            destination_emoji_with_urls = { name: val for name, val in self.emoji_dict().items() if not val.startswith("alias:") }
+
+            for name, url in destination_emoji_with_urls.items():
+                image = image_client.get(url)
+                digest = image_client.hexdigest(image)
+
+                if not self._hashes_for_emoji_images.get(digest):
+                    self._hashes_for_emoji_images[digest] = name
+
+        return self._hashes_for_emoji_images
