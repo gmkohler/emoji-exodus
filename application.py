76b97930909555dacc7f611991ec0fbd3beb1238
Gregory Kohler <kohler@squareup.com>
2019-08-16 11:58:43 -0700

    🦍🦛🐪🦌 support multiple emoji uploads
    made possible via refactor in https://github.com/gmkohler/emoji-exodus/pull/29

--- application.py
+++ application.py
@@ -2,10 +2,10 @@
 import os
 import sys
 from util.tokens import SOURCE_ENV_VARIABLE, DESTINATION_ENV_VARIABLE
-
 from emoji import emoji_transfer_service, emoji_service
 
 source_service = emoji_service(os.environ[SOURCE_ENV_VARIABLE])
 destination_service = emoji_service(os.environ[DESTINATION_ENV_VARIABLE])
 
-emoji_transfer_service.transfer(source_service.emoji_dict.emoji_dict, destination_service, sys.argv[1])
+for emoji_name in sys.argv[1:]:
+    emoji_transfer_service.transfer(source_service.emoji_dict.emoji_dict, destination_service, emoji_name)
