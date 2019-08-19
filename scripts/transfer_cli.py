b3e0380203c18f218700da172cf819eb77c75f13
Patrick Vilhena <pvilhena@squareup.com>
2019-08-19 15:39:52 -0700

    ⚠️ Fix warning around error raising in transfer_cli script

--- scripts/transfer_cli.py
+++ scripts/transfer_cli.py
@@ -5,17 +5,21 @@ import re
 
 from emoji import emoji_transfer_service, emoji_service, find_all_emoji_by_name
 from util.tokens import SOURCE_ENV_VARIABLE, DESTINATION_ENV_VARIABLE
-from emoji import emoji_transfer_service, emoji_service
+
+
+class InputError(Exception):
+    pass
+
 
 @click.command()
 @click.option('--source', default=None, help='A csv filename from which to import emoji')
 @click.argument('emoji_names', nargs=-1)
 def import_emoji(emoji_names, source):
     if not bool(source) ^ bool(emoji_names):
-        raise("You must either provide a list emoji_names or provide a CSV file via the --csv option")
+        raise InputError("You must either provide a list emoji_names or provide a CSV file via the --csv option")
+
     source_dict = emoji_service(os.environ[SOURCE_ENV_VARIABLE]).emoji_dict.emoji_dict
     destination_service = emoji_service(os.environ[DESTINATION_ENV_VARIABLE])
-
     if bool(source):
         emoji_names = _emoji_from_csv(source)
 
